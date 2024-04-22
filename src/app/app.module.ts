24085a8c9da24c48c903afb32047e55154331158
Irina Bubela <Iryna.Bubela@epam.com>
2024-04-22 23:52:49 +0200

    added navbar, fixing standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth/auth.service';
 
 const routes: Routes = [
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [],
+})
+  .catch((err) => console.log(err));
