773a69ecc541fc337bae1f5db523b9d3e2f52810
Irina Bubela <Iryna.Bubela@epam.com>
2024-09-19 22:39:47 +0200

    changes for charts

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import {
   ApexNonAxisChartSeries,
   ApexChart,
@@ -7,7 +7,9 @@ import {
   ApexLegend,
   NgxApexchartsModule
 } from 'ngx-apexcharts';
+import { MapService } from '../../services/map/map.service';
 
+const TOTAL_COUNTRIES = 195;
 
 @Component({
   selector: 'app-charts',
@@ -17,10 +19,12 @@ import {
   imports: [CommonModule, NgxApexchartsModule]
 })
 export class ChartsComponent implements OnInit {
-  public chartSeries: ApexNonAxisChartSeries = [50, 145]; // Visited and remaining countries
+  public chartSeries: ApexNonAxisChartSeries = [0, 145]; // Visited and remaining countries
   public chartLabels = ['Remaining Countries', 'Visited Countries'];
+  public totalCountOfCountries: number;
+  public remainingCountries: number;
 
-  public chartColors: string[] = ['#EAC452', '#000000'];
+  public chartColors: string[] = ['#EAC452', '#798086'];
 
   public chartOptions: ApexChart = {
     type: 'pie',
@@ -48,14 +52,29 @@ export class ChartsComponent implements OnInit {
     floating: false,
     fontSize: '14px',
     markers: {
-      strokeWidth: 30,  
-      fillColors: ['#000000', '#EAC452'], 
+      strokeWidth: 30,
+      fillColors: ['#61666b', '#EAC452'],
       offsetY: 0
     },
     offsetY: 10
   };
 
-  constructor() {}
+  constructor(
+    private readonly mapService: MapService,
+    private readonly cdRef: ChangeDetectorRef,
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getVisitedCountries();
+  }
+
+  public getVisitedCountries(): void {
+    this.mapService.getTotalCountriesSelected()
+      .subscribe(totalNum => {
+        this.totalCountOfCountries = totalNum;
+        this.remainingCountries = TOTAL_COUNTRIES - this.totalCountOfCountries;
+        this.chartSeries = [this.totalCountOfCountries, this.remainingCountries]
+        this.cdRef.detectChanges();
+      });
+  }
 }
